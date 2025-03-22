ed17ac279d8b925528ad4c995d28532b0b20dba4
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2025-03-22 23:24:28 +0900

    chore: 멀티 모듈 구조에서 단일 모듈 구조로 전환 (#3)

---
+++ src/main/java/com/topay/TopayApplication.java
@@ -0,0 +1,12 @@
+package com.topay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopayApplication.class, args);
+    }
+}
