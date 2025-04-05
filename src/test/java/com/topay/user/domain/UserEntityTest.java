a4bb3abdd83cd4e03f7c4a6931a391474b531afe
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2025-04-05 10:58:55 +0900

    test: UseEntity 객체 생성 테스트 로직 수정 - password 암호화 로직은 별도의 테스트에서 수행

--- src/test/java/com/topay/user/domain/UserEntityTest.java
+++ src/test/java/com/topay/user/domain/UserEntityTest.java
@@ -11,10 +11,6 @@ class UserEntityTest {
 
     private String name;
     private String email;
-
-    // TODO: 암복화 처리 -> 단위 테스트
-    private String password;
-    
     private Account account;
     private Level level;
 
@@ -34,12 +30,11 @@ class UserEntityTest {
         UserEntity user = UserEntity.of(name, email, account, level);
 
         // then
-        assertNull(user.getId(), "새로 생성된 UserEntity의 id는 null이어야 합니다.");
-        assertEquals(name, user.getName(), "이름이 일치해야 합니다.");
-        assertEquals(email, user.getEmail(), "이메일이 일치해야 합니다.");
-        assertEquals(password, user.getPassword(), "비밀번호가 일치해야 합니다.");
-        assertEquals(account, user.getAccount(), "계좌 정보가 일치해야 합니다.");
-        assertEquals(level, user.getLevel(), "레벨이 일치해야 합니다.");
+        assertNull(user.getId(), "새로 생성된 UserEntity의 id는 null이어야 한다.");
+        assertEquals(name, user.getName(), "이름이 일치해야 한다.");
+        assertEquals(email, user.getEmail(), "이메일이 일치해야 한다.");
+        assertEquals(account, user.getAccount(), "계좌 정보가 일치해야 한다.");
+        assertEquals(level, user.getLevel(), "레벨이 일치해야 한다.");
     }
 
 }
